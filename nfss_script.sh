1540d11a1b503e22175fa838df22a52ddc4cf93f
root <root@localhost.localdomain>
2020-08-19 11:30:50 +0300

    Update config

--- nfss_script.sh
+++ nfss_script.sh
@@ -3,12 +3,14 @@ yum install nfs-utils -y
 mkdir /mnt/nfs_share
 mkdir /mnt/nfs_share/upload
 chmod -R 777 /mnt
-sudo systemctl start firewalld
-sudo firewall-cmd --permanent --zone=public --add-service=nfs
-sudo firewall-cmd --permanent --zone=public --add-service=mountd
-sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
-sudo firewall-cmd --zone=public --add-port=2049/udp --permanent
-sudo firewall-cmd --zone=public --add-port=20048/udp --permanent
-sudo firewall-cmd --reload
-sudo sh -c "echo '/mnt/nfs_share 192.168.50.11(rw,sync,root_squash,no_all_squash)' >> /etc/exports"
+systemctl start firewalld
+firewall-cmd --permanent --zone=public --add-service=nfs
+firewall-cmd --permanent --zone=public --add-service=mountd
+firewall-cmd --permanent --zone=public --add-service=rpc-bind
+firewall-cmd --zone=public --add-port=2049/udp --permanent
+firewall-cmd --zone=public --add-port=20048/udp --permanent
+firewall-cmd --reload
+systemctl enable rpcbind nfs-server
+systemctl start rpcbind nfs-server
+echo "/mnt/nfs_share 192.168.50.11(rw,sync,root_squash,no_all_squash)" >> /etc/exports
 exportfs -r
